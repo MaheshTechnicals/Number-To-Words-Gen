7f7d4e70afd38e124c3cc3373082a1acdf2fbd94
msvarma10001@gmail.com <msvarma10001@gmail.com>
2023-08-18 12:41:42 +0530

    Added some js code

---
+++ main.js
@@ -0,0 +1,27 @@
+let input = document.querySelector("#input")
+let btn = document.querySelector("#btn")
+
+btn.addEventListener("click", () => {
+
+  num2word()
+
+})
+
+
+
+
+
+let num2word = () => {
+  const n2w = numberToWords
+  if (input.value == "") {
+    console.log("Please Enter Valid Number")
+  }
+  else {
+    console.log("bye")
+  }
+
+
+
+
+
+}
